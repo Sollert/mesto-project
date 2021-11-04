d9cb6c035869d6a6822e3e0cae7297dccb8cd018
Sergey Maximyuk <smaksimiuk97@gmail.com>
2021-11-04 05:41:37 +0300

    Добавил открытие/закрытие попапа редактирования профиля

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// Открытие окна редактирования профиля по клику на кнопку редактирования профиля
+const buttonOpenEditProfile = document.querySelector('.user__edit-button');
+buttonOpenEditProfile.addEventListener('click', function(){
+  editProfilePopup.classList.add('popup_opened')
+})
+
+// Закрытие окна редактирования профиля по клику на крестик
+const buttonCloseEditProfile = document.querySelector('.popup__close-button')
+buttonCloseEditProfile.addEventListener('click', function(){
+  editProfilePopup.classList.remove('popup_opened')
+})
