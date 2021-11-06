f3988e1c626b173e75ff05d91830e32032943366
Sergey Maximyuk <smaksimiuk97@gmail.com>
2021-11-06 17:21:56 +0300

    Релизуем сохранение данных из формы редактирования профиля на странице

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,9 @@ buttonCloseEditProfile.addEventListener('click', function(){
 })
 
 // Подставляем в value инпутов редактирования профиля значения со страницы
-const userNameInput = editProfilePopup.querySelector('[name = username]')
-const userStatusInput = editProfilePopup.querySelector('[name = userstatus]')
+const editProfileForm = editProfilePopup.querySelector('.form')
+const userNameInput = editProfileForm.querySelector('[name = username]')
+const userStatusInput = editProfileForm.querySelector('[name = userstatus]')
 const userName = profile.querySelector('.user__name')
 const userStatus = profile.querySelector('.user__status')
 
@@ -27,3 +28,14 @@ buttonCloseEditProfile.addEventListener('click', function(){
   userNameInput.value = userName.textContent
   userStatusInput.value = userStatus.textContent
 })
+
+
+// Реализуем сохранение данных из формы
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  userName.textContent = userNameInput.value;
+  userStatus.textContent = userStatusInput.value;
+  editProfilePopup.classList.remove('popup_opened')
+}
+
+editProfileForm.addEventListener('submit', formSubmitHandler); 
