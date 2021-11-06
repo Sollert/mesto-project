3ed3400d1b7816e5e68eb6ec214e7f449dbab737
Sergey Maximyuk <smaksimiuk97@gmail.com>
2021-11-06 16:20:52 +0300

    Релизуем поставление актуального имени и статуса в value инпутов редактирования профиля

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-// Открытие окна редактирования профиля по клику на кнопку редактирования профиля
-const buttonOpenEditProfile = document.querySelector('.user__edit-button');
+const profile = document.querySelector('.profile') // Объявляем переменную профиля
+const editProfilePopup = document.querySelector('#editProfilePopup') // Объявляем переменную попапа редактирования профиля
+
+// Реализуем открытие окна редактирования профиля по клику на кнопку редактирования профиля
+const buttonOpenEditProfile = profile.querySelector('.user__edit-button');
 buttonOpenEditProfile.addEventListener('click', function(){
   editProfilePopup.classList.add('popup_opened')
 })
 
-// Закрытие окна редактирования профиля по клику на крестик
+// Реализуем закрытие окна редактирования профиля по клику на крестик
 const buttonCloseEditProfile = editProfilePopup.querySelector('.popup__close-button')
 buttonCloseEditProfile.addEventListener('click', function(){
   editProfilePopup.classList.remove('popup_opened')
+})
+
+// Подставляем в value инпутов редактирования профиля значения со страницы
+const userNameInput = editProfilePopup.querySelector('[name = username]')
+const userStatusInput = editProfilePopup.querySelector('[name = userstatus]')
+const userName = profile.querySelector('.user__name')
+const userStatus = profile.querySelector('.user__status')
+
+userNameInput.value = userName.textContent
+userStatusInput.value = userStatus.textContent
+
+// Реализуем сохранение исходных значений value инпутов при закрытии попапа, нажатием на крестик
+buttonCloseEditProfile.addEventListener('click', function(){
+  userNameInput.value = userName.textContent
+  userStatusInput.value = userStatus.textContent
 })
