dafe516d73422cfcd185c5a837117737499825d8
Vasileva Anna <vann2299@yandex.ru>
2022-02-16 14:10:13 +0300

    Начала применять Валидацию в index.js

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -1,167 +1,155 @@
 export default class FormValidation {
-    constructor({ obj }, formElement){
-        this._formElement = formElement;
-        this._configValidation = obj;
-    }
+  constructor({ obj }, formElement) {
+    this._formElement = formElement;
+    this._configValidation = obj;
+  }
 
-    _showInputError (inputElement, inputInvalidClass, errorElement, errorClass, errorMessage) {
+  _showInputError(inputElement, inputInvalidClass, errorElement, errorClass, errorMessage) {
     inputElement.classList.add(inputInvalidClass);
     errorElement.classList.add(errorClass);
     errorElement.textContent = errorMessage;
-};
+  }
 
-    _hideInputError (inputElement, inputInvalidClass, errorElement, errorClass) {
+  _hideInputError(inputElement, inputInvalidClass, errorElement, errorClass) {
     inputElement.classList.remove(inputInvalidClass);
     errorElement.classList.remove(errorClass);
-    errorElement.textContent = '';
-};
+    errorElement.textContent = "";
+  }
 
-    _checkInputValidity (formElement, inputElement, inputInvalidClass, errorClass) {
-    const errorElement = formElement.querySelector(`#error-${inputElement.id}`)
+  _checkInputValidity(formElement, inputElement, inputInvalidClass, errorClass) {
+    const errorElement = formElement.querySelector(`#error-${inputElement.id}`);
     if (inputElement.validity.valid) {
-        hideInputError(inputElement, inputInvalidClass, errorElement, errorClass)
+      hideInputError(inputElement, inputInvalidClass, errorElement, errorClass);
     } else {
-        showInputError(inputElement, inputInvalidClass, errorElement, errorClass, inputElement.validationMessage);
+      showInputError(inputElement, inputInvalidClass, errorElement, errorClass, inputElement.validationMessage);
     }
-};
+  }
 
-    _hasInvalidInput (inputList) {
-    return inputList.some(inputElement => {
-        return !inputElement.validity.valid;
+  _hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
     });
-};
+  }
 
-    _disableButton (buttonElement, buttonDisabledClass) {
-    buttonElement.classList.add(buttonDisabledClass)
-    buttonElement.disabled = true
-}
+  _disableButton(buttonElement, buttonDisabledClass) {
+    buttonElement.classList.add(buttonDisabledClass);
+    buttonElement.disabled = true;
+  }
 
-    _enableButton (buttonElement, buttonDisabledClass) {
-    buttonElement.classList.remove(buttonDisabledClass)
-    buttonElement.disabled = false
-}
+  _enableButton(buttonElement, buttonDisabledClass) {
+    buttonElement.classList.remove(buttonDisabledClass);
+    buttonElement.disabled = false;
+  }
 
-    _toggleButtonState (formElement, inputList, buttonSelector, buttonDisabledClass) {
-    const buttonElement = formElement.querySelector(buttonSelector)
+  _toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass) {
+    const buttonElement = formElement.querySelector(buttonSelector);
 
     if (hasInvalidInput(inputList)) {
-        disableButton(buttonElement, buttonDisabledClass)
+      disableButton(buttonElement, buttonDisabledClass);
     } else {
-        enableButton(buttonElement, buttonDisabledClass)
+      enableButton(buttonElement, buttonDisabledClass);
     }
-};
+  }
 
-    _setEventListeners (formElement, { inputSelector, inputInvalidClass, errorClass, buttonSelector, buttonDisabledClass }) {
-    const inputList = Array.from(formElement.querySelectorAll(inputSelector))
-    inputList.forEach(inputElement => {
-        inputElement.addEventListener('input', () => {
-            checkInputValidity(formElement, inputElement, inputInvalidClass, errorClass);
-            toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
-        });
+  _setEventListeners(formElement, { inputSelector, inputInvalidClass, errorClass, buttonSelector, buttonDisabledClass }) {
+    const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        checkInputValidity(formElement, inputElement, inputInvalidClass, errorClass);
+        toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
+      });
     });
 
     toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
-};
+  }
 
-    enableValidation ({ formSelector, ...rest }) {
-    Array.from(document.querySelectorAll(formSelector)).forEach(formElement => {
-        formElement.addEventListener('submit', (event) => {
-            event.preventDefault();
-        });
+  enableValidation({ formSelector, ...rest }) {
+    Array.from(document.querySelectorAll(formSelector)).forEach((formElement) => {
+      formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+      });
 
-        setEventListeners(formElement, rest);
+      setEventListeners(formElement, rest);
     });
-};
-
-
+  }
 }
 
 // ПОКАЗАТЬ ОШИБКИ
 const showInputError = (inputElement, inputInvalidClass, errorElement, errorClass, errorMessage) => {
-    inputElement.classList.add(inputInvalidClass);
-    errorElement.classList.add(errorClass);
-    errorElement.textContent = errorMessage;
+  inputElement.classList.add(inputInvalidClass);
+  errorElement.classList.add(errorClass);
+  errorElement.textContent = errorMessage;
 };
 
 // СКРЫТЬ ОШИБКИ
 const hideInputError = (inputElement, inputInvalidClass, errorElement, errorClass) => {
-    inputElement.classList.remove(inputInvalidClass);
-    errorElement.classList.remove(errorClass);
-    errorElement.textContent = '';
+  inputElement.classList.remove(inputInvalidClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = "";
 };
 
 // ВАЛИДАЦИЯ
 const checkInputValidity = (formElement, inputElement, inputInvalidClass, errorClass) => {
-    const errorElement = formElement.querySelector(`#error-${inputElement.id}`)
-    if (inputElement.validity.valid) {
-        hideInputError(inputElement, inputInvalidClass, errorElement, errorClass)
-    } else {
-        showInputError(inputElement, inputInvalidClass, errorElement, errorClass, inputElement.validationMessage);
-    }
+  const errorElement = formElement.querySelector(`#error-${inputElement.id}`);
+  if (inputElement.validity.valid) {
+    hideInputError(inputElement, inputInvalidClass, errorElement, errorClass);
+  } else {
+    showInputError(inputElement, inputInvalidClass, errorElement, errorClass, inputElement.validationMessage);
+  }
 };
 
 // ПРОВЕРИТЬ ЕСТЬ ЛИ НЕПРАВИЛЬНО ЗАПОЛНЕННЫЕ ИНПУТЫ
 const hasInvalidInput = (inputList) => {
-    return inputList.some(inputElement => {
-        return !inputElement.validity.valid;
-    });
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
 };
 
 // СДЕЛАТЬ КНОПКУ НЕАКТИВНОЙ
 const disableButton = (buttonElement, buttonDisabledClass) => {
-    buttonElement.classList.add(buttonDisabledClass)
-    buttonElement.disabled = true
-}
+  buttonElement.classList.add(buttonDisabledClass);
+  buttonElement.disabled = true;
+};
 
 // СДЕЛАТЬ КНОПКУ АКТИВНОЙ
 const enableButton = (buttonElement, buttonDisabledClass) => {
-    buttonElement.classList.remove(buttonDisabledClass)
-    buttonElement.disabled = false
-}
+  buttonElement.classList.remove(buttonDisabledClass);
+  buttonElement.disabled = false;
+};
 
 // МЕНЯТЬ АКТИВНОСТЬ КНОПКИ
 const toggleButtonState = (formElement, inputList, buttonSelector, buttonDisabledClass) => {
-    const buttonElement = formElement.querySelector(buttonSelector)
+  const buttonElement = formElement.querySelector(buttonSelector);
 
-    if (hasInvalidInput(inputList)) {
-        disableButton(buttonElement, buttonDisabledClass)
-    } else {
-        enableButton(buttonElement, buttonDisabledClass)
-    }
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonElement, buttonDisabledClass);
+  } else {
+    enableButton(buttonElement, buttonDisabledClass);
+  }
 };
 
 // ДОБАВИТЬ СЛУШАТЕЛИ
 const setEventListeners = (formElement, { inputSelector, inputInvalidClass, errorClass, buttonSelector, buttonDisabledClass }) => {
-    const inputList = Array.from(formElement.querySelectorAll(inputSelector))
-    inputList.forEach(inputElement => {
-        inputElement.addEventListener('input', () => {
-            checkInputValidity(formElement, inputElement, inputInvalidClass, errorClass);
-            toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
-        });
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement, inputInvalidClass, errorClass);
+      toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
     });
+  });
 
-    toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
+  toggleButtonState(formElement, inputList, buttonSelector, buttonDisabledClass);
 };
 
 // РАЗРЕШИТЬ ВАЛИДАЦИЮ
 const enableValidation = ({ formSelector, ...rest }) => {
-    Array.from(document.querySelectorAll(formSelector)).forEach(formElement => {
-        formElement.addEventListener('submit', (event) => {
-            event.preventDefault();
-        });
-
-        setEventListeners(formElement, rest);
+  Array.from(document.querySelectorAll(formSelector)).forEach((formElement) => {
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
     });
+
+    setEventListeners(formElement, rest);
+  });
 };
 
-
-export {
-    showInputError,
-    hideInputError,
-    checkInputValidity,
-    hasInvalidInput,
-    disableButton,
-    enableButton,
-    setEventListeners,
-    enableValidation
-}
+export { showInputError, hideInputError, checkInputValidity, hasInvalidInput, disableButton, enableButton, setEventListeners, enableValidation };
--- src/components/index.js
+++ src/components/index.js
@@ -1,188 +1,166 @@
 // ИМПОРТ INDEX.CSS
-import '../pages/index.css'
+import "../pages/index.css";
 // ИМПОРТЫ КЛАССОВ
-import Api from './Api.js'
-import Card from './Card.js'
-import Popup from './Popup.js'
-import PopupWithForm from './PopupWithForm.js'
-import PopupWithImage from './PopupWithImage.js'
-import Section from './Section.js'
-import UserInfo from './UserInfo.js'
-import FormValidation from './FormValidation.js'
+import Api from "./Api.js";
+import Card from "./Card.js";
+import Popup from "./Popup.js";
+import PopupWithForm from "./PopupWithForm.js";
+import PopupWithImage from "./PopupWithImage.js";
+import Section from "./Section.js";
+import UserInfo from "./UserInfo.js";
+import FormValidation from "./FormValidation.js";
 // ИМПОРТ CONSTANTS.JS
-import {
-    popupEditProfile,
-    buttonOpenPopupEditProfile,
-    formEditProfile,
-    userNameInput,
-    userStatusInput,
-    userName,
-    userStatus,
-    editProfileSaveButton,
-    cardsContainer,
-    popupAddCard,
-    buttonOpenPopupAddCard,
-    addCardForm,
-    userAvatar,
-    avatarPopup,
-    buttonOpenAvatarPopup,
-    addCardSaveButton,
-    editAvatarSaveButton,
-    editAvatarForm,
-    avatarLinkInput,
-    popups,
-    configApi,
-    configValidation,
-    templateSelector
-} from './constants.js'
+import { popupEditProfile, buttonOpenPopupEditProfile, formEditProfile, userNameInput, userStatusInput, userName, userStatus, editProfileSaveButton, cardsContainer, popupAddCard, buttonOpenPopupAddCard, addCardForm, userAvatar, avatarPopup, buttonOpenAvatarPopup, addCardSaveButton, editAvatarSaveButton, editAvatarForm, avatarLinkInput, popups, configApi, configValidation, templateSelector } from "./constants.js";
 
-let userId = '73716017333b67513d746598' // Сейчас нужен для хардкода, после того как будем доставать юзеринфо удалить
+let userId = "73716017333b67513d746598"; // Сейчас нужен для хардкода, после того как будем доставать юзеринфо удалить
 
 // ОБЪЯВИТЬ ЭКЗЕМПЛЯР АПИ ДЛЯ ВСЕГО
-const api = new Api(configApi)
+const api = new Api(configApi);
 
 // СОБЫТИЯ ДЛЯ КАРТОЧКИ
 // Открыть попап с изображением
-const handleCardClick = () => {
-
-}
+const handleCardClick = () => {};
 
 // Удалить карточку
-const handleRemoveCard = () => {
-
-}
+const handleRemoveCard = () => {};
 
 // Лайк
 const addLike = (card, cardId) => {
-    const cardLike = card.querySelector('.card__like')
-    const likeCounter = card.querySelector('.card__like-count')
+  const cardLike = card.querySelector(".card__like");
+  const likeCounter = card.querySelector(".card__like-count");
 
-    api.addLikeCard(cardId)
-        .then((data) => {
-            cardLike.classList.add('card__like_active')
-            likeCounter.textContent = data.likes.length;
-        })
-}
+  api.addLikeCard(cardId).then((data) => {
+    cardLike.classList.add("card__like_active");
+    likeCounter.textContent = data.likes.length;
+  });
+};
 
 // Дизлайк
 const removeLike = (card, cardId) => {
-    const cardLike = card.querySelector('.card__like')
-    const likeCounter = card.querySelector('.card__like-count')
+  const cardLike = card.querySelector(".card__like");
+  const likeCounter = card.querySelector(".card__like-count");
 
-    api.removeLikeCard(cardId)
-        .then((data) => {
-            cardLike.classList.remove('card__like_active')
-            likeCounter.textContent = data.likes.length;
-        })
-}
+  api.removeLikeCard(cardId).then((data) => {
+    cardLike.classList.remove("card__like_active");
+    likeCounter.textContent = data.likes.length;
+  });
+};
 
 // ОБЪЯИТЬ ЭКЗЕМПЛЯР SECTION ДЛЯ ДОБАВЛЕНИЯ КАРТОЧЕК В РАЗМЕТКУ
-const cardList = new Section((data) => renderCard(data), '.cards');
+const cardList = new Section((data) => renderCard(data), ".cards");
 
 // РЕНДЕР КАРТОЧЕК
 const renderCard = (data) => {
-    const card = new Card(data, templateSelector, handleCardClick, handleRemoveCard, addLike, removeLike)
-    const cardElement = card.generate(userId)
-    cardList.setItem(cardElement)
-}
+  const card = new Card(data, templateSelector, handleCardClick, handleRemoveCard, addLike, removeLike);
+  const cardElement = card.generate(userId);
+  cardList.setItem(cardElement);
+};
 
 // ОТОБРАЗИТЬ ДАННЫЕ С СЕРВЕРА НА СТРАНИЦУ
 const loadAllInfo = () => {
-    Promise.all([api.getUserInfo(), api.getInitialCards()])
-        .then(([user, cardsList]) => {
-            userName.textContent = user.name
-            userStatus.textContent = user.about
-            userAvatar.src = user.avatar
+  Promise.all([api.getUserInfo(), api.getInitialCards()])
+    .then(([user, cardsList]) => {
+      userName.textContent = user.name;
+      userStatus.textContent = user.about;
+      userAvatar.src = user.avatar;
 
-            cardsList.reverse()
-            cardsList.forEach(card => {
-                renderCard(card)
-                console.log(card)
-            })
-        })
-        .catch((err) => {
-            console.log(`Ошибка: ${err}`);
-        })
-}
-loadAllInfo()
+      cardsList.reverse();
+      cardsList.forEach((card) => {
+        renderCard(card);
+        console.log(card);
+      });
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+    });
+};
+loadAllInfo();
 
 // ПОДСТАВЛЯТЬ В VALUE ФОРМЫ ЮЗЕРА АКТУАЛЬНЫЕ ДАННЫЕ
 const putUserInfo = () => {
-    userNameInput.value = userName.textContent
-    userStatusInput.value = userStatus.textContent
-}
+  userNameInput.value = userName.textContent;
+  userStatusInput.value = userStatus.textContent;
+};
 
 // СЛУШАТЕЛИ
 const setListeners = () => {
+  // ДОБАВИТЬ КАРТОЧКУ
+  addCardForm.addEventListener("submit", () => {
+    addCardSaveButton.textContent = "Сохранение...";
+    addCard();
+  });
 
-    // ДОБАВИТЬ КАРТОЧКУ
-    addCardForm.addEventListener('submit', () => {
-        addCardSaveButton.textContent = 'Сохранение...'
-        addCard()
-    })
+  // СОХРАНЯТЬ ДАННЫЕ ИЗ ФОРМЫ РЕДАКТИРОВАНИЯ ПРОФИЛЯ
+  formEditProfile.addEventListener("submit", (evt) => {
+    editProfileSaveButton.textContent = "Сохранение...";
+    updateUserInfo(userNameInput.value, userStatusInput.value)
+      .then((res) => {
+        userName.textContent = userNameInput.value;
+        userStatus.textContent = userStatusInput.value;
+        closePopup(popupEditProfile);
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      })
+      .finally(() => {
+        editProfileSaveButton.textContent = "Сохранить";
+      });
+  });
 
-    // СОХРАНЯТЬ ДАННЫЕ ИЗ ФОРМЫ РЕДАКТИРОВАНИЯ ПРОФИЛЯ
-    formEditProfile.addEventListener('submit', (evt) => {
-        editProfileSaveButton.textContent = 'Сохранение...'
-        updateUserInfo(userNameInput.value, userStatusInput.value)
-            .then((res) => {
-                userName.textContent = userNameInput.value
-                userStatus.textContent = userStatusInput.value
-                closePopup(popupEditProfile)
-            })
-            .catch((err) => {
-                console.log(`Ошибка: ${err}`)
-            })
-            .finally(() => {
-                editProfileSaveButton.textContent = 'Сохранить'
-            })
-    })
+  // СОХРАНЯТЬ ДАННЫЕ ИЗ ФОРМЫ ОБНОВИТЬ АВАТАР
+  editAvatarForm.addEventListener("submit", (evt) => {
+    editAvatarSaveButton.textContent = "Сохранение...";
+    updateAvatar(avatarLinkInput.value)
+      .then((res) => {
+        userAvatar.src = res.avatar;
+        editAvatarForm.reset();
+        closePopup(avatarPopup);
+        disableButton(editAvatarSaveButton, "form__save-button_disabled");
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      })
+      .finally(() => {
+        editAvatarSaveButton.textContent = "Сохранить";
+      });
+  });
 
-    // СОХРАНЯТЬ ДАННЫЕ ИЗ ФОРМЫ ОБНОВИТЬ АВАТАР
-    editAvatarForm.addEventListener('submit', (evt) => {
-        editAvatarSaveButton.textContent = 'Сохранение...'
-        updateAvatar(avatarLinkInput.value)
-            .then((res) => {
-                userAvatar.src = res.avatar
-                editAvatarForm.reset()
-                closePopup(avatarPopup)
-                disableButton(editAvatarSaveButton, 'form__save-button_disabled')
-            })
-            .catch((err) => {
-                console.log(`Ошибка: ${err}`)
-            })
-            .finally(() => {
-                editAvatarSaveButton.textContent = 'Сохранить'
-            })
-    })
+  // ОТКРЫТЬ ПОПАП ПРОФИЛЯ
+  buttonOpenPopupEditProfile.addEventListener("click", () => {
+    openPopup(popupEditProfile);
+    putUserInfo();
+  });
 
-    // ОТКРЫТЬ ПОПАП ПРОФИЛЯ
-    buttonOpenPopupEditProfile.addEventListener('click', () => {
-        openPopup(popupEditProfile)
-        putUserInfo()
-    })
+  // ОТКРЫТЬ ПОПАП РЕДАКТИРОВАНИЯ АВАТАРА
+  buttonOpenAvatarPopup.addEventListener("click", () => openPopup(avatarPopup));
 
-    // ОТКРЫТЬ ПОПАП РЕДАКТИРОВАНИЯ АВАТАРА
-    buttonOpenAvatarPopup.addEventListener('click', () => openPopup(avatarPopup))
+  // ОТКРЫТЬ ПОПАП ЗАГРУЗКИ КАРТОЧКИ
+  buttonOpenPopupAddCard.addEventListener("click", () => openPopup(popupAddCard));
 
-    // ОТКРЫТЬ ПОПАП ЗАГРУЗКИ КАРТОЧКИ
-    buttonOpenPopupAddCard.addEventListener('click', () => openPopup(popupAddCard))
+  // ЗАКРЫТЬ ПОПАПЫ
+  popups.forEach((popup) => {
+    popup.addEventListener("mousedown", (evt) => {
+      if (evt.target.classList.contains("popup_opened")) {
+        closePopup(popup);
+      }
+      if (evt.target.classList.contains("popup__close-button")) {
+        closePopup(popup);
+      }
+    });
+  });
+};
+setListeners();
 
-    // ЗАКРЫТЬ ПОПАПЫ
-    popups.forEach((popup) => {
-        popup.addEventListener('mousedown', (evt) => {
-            if (evt.target.classList.contains('popup_opened')) {
-                closePopup(popup)
-            }
-            if (evt.target.classList.contains('popup__close-button')) {
-                closePopup(popup)
-            }
-        })
-    })
-}
-setListeners()
+//Валидация
+const enableVlidation = () => {
+  const validation = new FormValidation({ obj: configValidation }, popups);
+
+  popups.forEach((elem) => {
+    validation.enableValidation(configValidation);
+  });
+};
+
+enableVlidation();
 
 // ЭКСПОРТ
-export {
-  userId
-}
+export { userId };
