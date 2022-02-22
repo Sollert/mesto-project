3e0de5cbb7047e7b23fea61273eac88ebea4e638
Vasileva Anna <vann2299@yandex.ru>
2022-02-22 19:02:45 +0300

    Начали делать попап карточки и добавление карточки

--- src/components/index.js
+++ src/components/index.js
@@ -10,7 +10,7 @@ import Section from "./Section.js";
 import UserInfo from "./UserInfo.js";
 import FormValidation from "./FormValidation.js";
 // ИМПОРТ CONSTANTS.JS
-import {userNameInput, userStatusInput, userName, userStatus, buttonOpenPopupEditProfile, cardsContainer, popupAddCard, buttonOpenPopupAddCard, addCardForm, userAvatar, avatarPopup, buttonOpenAvatarPopup, addCardSaveButton, editAvatarSaveButton, editAvatarForm, avatarLinkInput, popups, configApi, configValidation, templateSelector } from "./constants.js";
+import { editProfileSaveButton, userNameInput, userStatusInput, userName, userStatus, buttonOpenPopupEditProfile, cardsContainer, popupAddCard, buttonOpenPopupAddCard, addCardForm, userAvatar, avatarPopup, buttonOpenAvatarPopup, addCardSaveButton, editAvatarSaveButton, editAvatarForm, avatarLinkInput, popups, configApi, configValidation, templateSelector } from "./constants.js";
 
 // ОБЪЯВИТЬ ЭКЗЕМПЛЯР АПИ ДЛЯ ВСЕГО
 const api = new Api(configApi);
@@ -187,21 +187,22 @@ popupWithAvatar.setEventListeners();
 
 //Popup Edit Profile
 const handlePopupWithProfile = (object) => {
-  buttonOpenPopupEditProfile.textContent = "Сохранение...";
-  api.updateUserInfo(object.username, object.userstatus)
-      .then((res) => {
-        userName.textContent = res.name;
-        userStatus.textContent = res.about;
-        popupEditProfile.closePopup();
-      })
-      .catch((err) => {
-        console.log(`Ошибка: ${err}`);
-      })
-      .finally(() => {
-        buttonOpenPopupEditProfile.textContent = "Сохранить";
-      });
-}
-const popupEditProfile = new PopupWithForm('#popup-edit-profile', handlePopupWithProfile);
+  editProfileSaveButton.textContent = "Сохранение...";
+  api
+    .updateUserInfo(object.username, object.userstatus)
+    .then((res) => {
+      userName.textContent = res.name;
+      userStatus.textContent = res.about;
+      popupEditProfile.closePopup();
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+    })
+    .finally(() => {
+      editProfileSaveButton.textContent = "Сохранить";
+    });
+};
+const popupEditProfile = new PopupWithForm("#popup-edit-profile", handlePopupWithProfile);
 popupEditProfile.setEventListeners();
 
 // ОТКРЫТЬ ПОПАП РЕДАКТИРОВАНИЯ АВАТАРА
@@ -210,13 +211,42 @@ buttonOpenAvatarPopup.addEventListener("click", () => {
 });
 
 // ОТКРЫТЬ ПОПАП ПРОФИЛЯ
-  buttonOpenPopupEditProfile.addEventListener("click", () => {
-   popupEditProfile.openPopup();
-   putUserInfo();
-  });
+buttonOpenPopupEditProfile.addEventListener("click", () => {
+  popupEditProfile.openPopup();
+  putUserInfo();
+});
 
 // ПОДСТАВЛЯТЬ В VALUE ФОРМЫ ЮЗЕРА АКТУАЛЬНЫЕ ДАННЫЕ
-  const putUserInfo = () => {
-    userNameInput.value = userInfo.getUserInfo().userName;
-    userStatusInput.value = userInfo.getUserInfo().userStatus;
-}
+const putUserInfo = () => {
+  userNameInput.value = userInfo.getUserInfo().userName;
+  userStatusInput.value = userInfo.getUserInfo().userStatus;
+
+  editProfileSaveButton.removeAttribute("disabled");
+  editProfileSaveButton.classList.remove("form__save-button_disabled");
+};
+
+// ПОПАП С ДОБАВЛЕНИЕМ КАРТОЧКИ
+
+const handlePopupAddCard = (object) => {
+  addCardSaveButton.textContent = "Сохранение...";
+  api
+    .loadCard(object.name, object.link)
+    .then((res) => {
+      const newCard = new Card(res, templateSelector, handleCardClick, handleRemoveCard, addLike, removeLike, isOwner, isLiked);
+      newCard.generate();
+      popupAddCardForm.closePopup();
+    })
+    .catch((err) => {
+      console.log(`Ошибка: ${err}`);
+    })
+    .finally(() => {
+      addCardSaveButton.textContent = "Сохранить";
+    });
+};
+
+const popupAddCardForm = new PopupWithForm("#popup-add-card", handlePopupAddCard);
+popupAddCardForm.setEventListeners();
+
+buttonOpenPopupAddCard.addEventListener("click", () => {
+  popupAddCardForm.openPopup();
+});
